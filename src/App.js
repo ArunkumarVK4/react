302580f974c81860637f0206549fa263493bcfa0
ArunkumarVK4 <vkarunkumar446@gmail.com>
2023-04-22 14:59:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import { Price } from './Price';
+
+function App() {
+  return (
+    <div className="App">
+      <Price/>
+    </div>
+  );
+}
+
+export default App;
+
+
+
---
+++ src/Price.js
@@ -0,0 +1,85 @@
+import './style.css';
+
+export function Price() {
+  return (
+    
+    <div class="container">
+      <div className='over'>
+      <div class="row">
+    
+   <div class='d-flex'>
+  
+    <div class="card-body ">
+      <h5 class="card-title text-center">
+        <p>FREE</p>
+        <p><>0</><sub>/month</sub></p>
+        <hr></hr>
+      </h5>
+      <p class="card-text">&#10003; Single User</p>
+      <p class="card-text">&#10003; 5GB Storage</p>
+      <p class="card-text">&#10003; Unlimited Public Projects</p>
+      <p class="card-text">&#10003; Community Access</p>
+      <p class="card-text text-muted">Unlimited Private Projects</p>
+      <p class="card-text text-muted">&#128473; Dedicated Phone Support</p>
+      <p class="card-text text-muted">&#128473; Free Subdomain</p>
+      <p class="card-text text-muted">&#128473; Monthly Status Reports</p>
+      <div class='text-center'>
+      <button type="button" class="btn btn-outline-primary ">Button</button>
+
+      </div>
+      
+
+    </div>
+  
+  
+    <div class="card-body ">
+      <h5 class="card-title text-center">
+        <p>PLUS</p>
+        <p><>$9</><sub>/month</sub></p>
+        <hr></hr>
+      </h5>
+      <p class="card-text">&#10003; 5 Users</p>
+      <p class="card-text">&#10003; 50GB Storage</p>
+      <p class="card-text">&#10003; Unlimited Public Projects</p>
+      <p class="card-text">&#10003; Community Access</p>
+      <p class="card-text">&#10003; Unlimited Private Projects</p>
+      <p class="card-text">&#10003; Dedicated Phone Support</p>
+      <p class="card-text">&#10003; Free Subdomain</p>
+      <p class="card-text text-muted">&#128473; Monthly Status Reports</p>
+      <div className='text-center'>
+      <button type="button" class="btn btn-outline-primary ">Button</button>
+      </div>
+      
+
+    </div>
+  
+
+    <div class="card-body ">
+      <h5 class="card-title text-center">
+        <p>PRO</p>
+        <p><>$49</><sub>/month</sub></p>
+        <hr></hr>
+      </h5>
+      <p class="card-text">&#10003; Single User</p>
+      <p class="card-text">&#10003; 5GB Storage</p>
+      <p class="card-text">&#10003; Unlimited Public Projects</p>
+      <p class="card-text">&#10003; Community Access</p>
+      <p class="card-text">&#10003; Unlimited Private Projects</p>
+      <p class="card-text">&#10003; Dedicated Phone Support</p>
+      <p class="card-text"><b>&#10003; Unlimited </b>Free Subdomain</p>
+      <p class="card-text">&#10003; Monthly Status Reports</p>
+      <div className='text-center'>
+      <button type="button" class="btn btn-outline-primary ">Button</button>
+      </div>
+      
+
+    </div>
+
+ 
+</div>
+</div>
+</div>
+    </div>
+  );
+}
+
