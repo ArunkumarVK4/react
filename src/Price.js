aa7c62e22fbe939afedab921d37184396de1594d
ArunkumarVK4 <vkarunkumar446@gmail.com>
2023-05-01 13:56:01 +0530

    final change

--- src/Price.js
+++ src/Price.js
@@ -1,85 +1,122 @@
-import './style.css';
+import "./style.css";
 
 export function Price() {
+  const data = [
+    {
+      title: "FREE",
+      price: "0",
+      data1: "Single User",
+      data2: "5GB Storage",
+      data3: "Unlimited Public Projects",
+      data4: "Community Access",
+      data5: "Unlimited Private Projects",
+      data6: "Dedicated Phone Support",
+      data7: "Free Subdomain",
+      data8: "Monthly Status Reports",
+    },
+    {
+      title: "PLUS",
+      price: "9",
+      data1: "5 User",
+      data2: "50GB Storage",
+      data3: "Unlimited Public Projects",
+      data4: "Community Access",
+      data5: "Unlimited Private Projects",
+      data6: "Dedicated Phone Support",
+      data7: "Free Subdomain",
+      data8: "Monthly Status Reports",
+    },
+    {
+      title: "FREE",
+      price: "49",
+      data1: "Unlimited  User",
+      data2: "150GB Storage",
+      data3: "Unlimited Public Projects",
+      data4: "Community Access",
+      data5: "Unlimited Private Projects",
+      data6: "Dedicated Phone Support",
+      data7: "Unlimited Free Subdomain",
+      data8: "Monthly Status Reports",
+    },
+  ];
+
   return (
-    
     <div className="container">
-      <div className='over'>
-      <div className="row">
-    
-   <div className='d-flex'>
-  
-    <div className="card-body ">
-      <h5 className="card-title text-center">
-        <p>FREE</p>
-        <p><>$0</><sub>/month</sub></p>
-        <hr></hr>
-      </h5>
-      <p className="card-text">&#10003; Single User</p>
-      <p className="card-text">&#10003; 5GB Storage</p>
-      <p className="card-text">&#10003; Unlimited Public Projects</p>
-      <p className="card-text">&#10003; Community Access</p>
-      <p className="card-text text-muted">Unlimited Private Projects</p>
-      <p className="card-text text-muted">&#128473; Dedicated Phone Support</p>
-      <p className="card-text text-muted">&#128473; Free Subdomain</p>
-      <p className="card-text text-muted">&#128473; Monthly Status Reports</p>
-      <div className='text-center'>
-      <button type="button" className="btn btn-outline-primary ">Button</button>
+      <div className="over">
+        <div className="row">
+          {data.map((e) => {
+            console.log(e.price);
 
+            return (
+              <div className="col-lg-4">
+                <div className="card-body p-5">
+                  <h5 className="card-title text-center">
+                    <p className="text-muted">{e.title}</p>
+                    <p>
+                      <span className="text">${e.price}</span>
+                      <sub>/month</sub>
+                    </p>
+                    <hr></hr>
+                  </h5>
+                  <p className="card-text">
+                    <i class="fa fa-check p-2" aria-hidden="true"></i>
+                    {e.data1}
+                  </p>
+                  <p className="card-text">
+                    <i class="fa fa-check p-2" aria-hidden="true"></i>
+                    {e.data2}
+                  </p>
+                  <p className="card-text">
+                    <i class="fa fa-check  p-2" aria-hidden="true"></i>
+                    {e.data3}
+                  </p>
+                  <p className="card-text">
+                    <i class="fa fa-check p-2" aria-hidden="true"></i>
+                    {e.data4}
+                  </p>
+                  <p className="card-text text-muted">
+                    {e.price == 49 || e.price == 9 ? (
+                      <i class="fa fa-check p-2" aria-hidden="true"></i>
+                    ) : (
+                      <i class="fa fa-times p-2" aria-hidden="true"></i>
+                    )}
+                    {e.data5}
+                  </p>
+                  <p className="card-text text-muted">
+                    {e.price == 49 || e.price == 9 ? (
+                      <i class="fa fa-check p-2" aria-hidden="true"></i>
+                    ) : (
+                      <i class="fa fa-times p-2" aria-hidden="true"></i>
+                    )}
+                    {e.data6}
+                  </p>
+                  <p className="card-text text-muted">
+                    {e.price == 49 ? (
+                      <i class="fa fa-check p-2" aria-hidden="true"></i>
+                    ) : (
+                      <i class="fa fa-times p-2" aria-hidden="true"></i>
+                    )}
+                    {e.data7}
+                  </p>
+                  <p className="card-text text-muted">
+                    {e.price == 49 ? (
+                      <i class="fa fa-check p-2" aria-hidden="true"></i>
+                    ) : (
+                      <i class="fa fa-times p-2" aria-hidden="true"></i>
+                    )}
+                    {e.data8}
+                  </p>
+                  <div className="text-center">
+                    <button type="button" className="btn btn-outline-primary ">
+                      Button
+                    </button>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      
-
-    </div>
-  
-  
-    <div className="card-body ">
-      <h5 className="card-title text-center">
-        <p>PLUS</p>
-        <p><>$9</><sub>/month</sub></p>
-        <hr></hr>
-      </h5>
-      <p className="card-text">&#10003; 5 Users</p>
-      <p className="card-text">&#10003; 50GB Storage</p>
-      <p className="card-text">&#10003; Unlimited Public Projects</p>
-      <p className="card-text">&#10003; Community Access</p>
-      <p className="card-text">&#10003; Unlimited Private Projects</p>
-      <p className="card-text">&#10003; Dedicated Phone Support</p>
-      <p className="card-text">&#10003; Free Subdomain</p>
-      <p className="card-text text-muted">&#128473; Monthly Status Reports</p>
-      <div className='text-center'>
-      <button type="button" className="btn btn-outline-primary ">Button</button>
-      </div>
-      
-
-    </div>
-  
-
-    <div className="card-body ">
-      <h5 className="card-title text-center">
-        <p>PRO</p>
-        <p><>$49</><sub>/month</sub></p>
-        <hr></hr>
-      </h5>
-      <p className="card-text">&#10003; Single User</p>
-      <p className="card-text">&#10003; 5GB Storage</p>
-      <p className="card-text">&#10003; Unlimited Public Projects</p>
-      <p className="card-text">&#10003; Community Access</p>
-      <p className="card-text">&#10003; Unlimited Private Projects</p>
-      <p className="card-text">&#10003; Dedicated Phone Support</p>
-      <p className="card-text"><b>&#10003; Unlimited </b>Free Subdomain</p>
-      <p className="card-text">&#10003; Monthly Status Reports</p>
-      <div className='text-center'>
-      <button type="button" className="btn btn-outline-primary ">Button</button>
-      </div>
-      
-
-    </div>
-
- 
-</div>
-</div>
-</div>
     </div>
   );
 }
-
